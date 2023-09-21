2f9c987f613b8aa1e42b6f2b622e1a0f9ce1a9ad
udevs <45226508+udevsharold@users.noreply.github.com>
2023-09-21 11:47:21 +0800

    -obsolete

--- obsolete/BakgrunnurServer.h
+++
@@ -1,9 +0,0 @@
-#import <RocketBootstrap/rocketbootstrap.h>
-
-@interface BakgrunnurServer : NSObject{
-    CPDistributedMessagingCenter * _messagingCenter;
-}
-+ (instancetype)sharedInstance;
--(NSDictionary *)terminateProcess:(NSString *)name withUserInfo:(NSDictionary *)userInfo;
-@end
-
